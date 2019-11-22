6857687ec9834ca4125f040acaeadd14768e95eb
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-11-21 21:47:06 -0300

    Update news.php

--- news.php
+++ news.php
@@ -9,7 +9,73 @@
 
     <div class="row justify-content-center">
         <div class="col-lg-4 offset-lg-1 col-md-4">
-            <h2>Sub Título</h2>
+<!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ExemploModalCentralizado">
+  
+Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="TituloModalCentralizado"> News</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      <h1>Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra </h1>
+      <figure>
+        <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
+        <figcaption>
+            Foto Fauno Cultural/Divulgação.
+        </figcaption>
+    </figure>
+        <div class=" mt-4 mb-4">
+
+        <p> 
+        Uma iniciativa inédita de um coletivo de produtores culturais e artistas irá transformar ruas, praças e avenidas de Belo Horizonte em um galeria de arte a céu aberto. Neste sábado (7), das 10h às 19h, será a estreia do Museu de Rua, que pretende converter espaços públicos degradados em imensos murais, locais de contemplação, ocupação e convivência. A primeira ação será realizada na rua Américo Scott, entre as ruas Edgar Coelho e Monte Alegre, no bairro Serra (confira aqui a página do evento).
+        </p>
+        <p>
+        Os 70 metros de comprimento do muro serão preenchidos simultaneamente com tinta spray a partir dos traços de 20 artistas. Dentre eles estão Goma, Clara Valente, Gud Assis, Ataíde Miranda, Testa e Carol. Além da pintura do muro, evento também terá barraquinhas com música boa e feirinha de produtores locais, como artesanato, roupas (o evento tem uma parceria com a Ancestral para fornecer blusas), bancada de flores, comidas naturais e exposição de arte.
+        </p>
+        <p>
+        A proposta é manter uma agenda constante de intervenções na cidade. O segundo evento já está marcado para o dia 21 de abril, também na Serra, desta vez na Praça Violeta Soter. Na sequência, a ideia é levar a ação para outras regiões da cidade. Para isso, o Museu de Rua lançou um financiamento coletivo (acesse aqui) para levantar recursos.
+        </p>
+        <p>
+        Os valores dos apoios vão de R$ 10 até R$ 5 mil. Como recompensa, há os mais variados produtos e serviços, como agradecimentos públicos no dia do evento, CDs de bandas independentes, garrafa da bebida Xeque-Mate, bolsa unisex da Real Grapixo, livro "A Cultura do Grafite. Por um direito das Ruas", de Mariana Gontijo, camisas, lata de spray grafitada por Ramar Gama, bonés, ingressos na Festa Sinestésica, aulas de Yoga, oficina de Graffiti, discotecagem, anuidade do plano de benefícios da empresa ForSócios, telas artísticas e murais.
+        </p>
+        <p>
+        O projeto foi idealizado pelo artista de rua Ramar Gama e pelo produtor cultural Ivan Neves. A eles, se juntaram o também artista de rua João Marcelo Goma (conhecido como Goma) e os produtores culturais Daniel Moreira, da ForSócios; e Gabriel Camargos, João Pedro Illanes e Pedro Carias, da festa Sinestésica.
+        </p>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/DjuCBJNqpWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+    </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+    <!--        <h2>Sub Título</h2>
             <p>Austro securae derecti quicquam toto plagae mare praecipites feras.</p>
         </div>
         <div class="col-lg-4 offset-lg-1 col-md-4">
@@ -19,13 +85,7 @@
     </div>
 
     <h2>Museu de Rua </h2>
-    <figure>
-        <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
-        <figcaption>
-            <a href="https://www.hojeemdia.com.br/museu-de-rua-inaugura-interven%C3%A7%C3%B5es-em-bh-ao-grafitar-muro-de-70-metros-na-serra-1.611203">Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra</a>
-
-        </figcaption>
-    </figure>
+    
     <h2>Sub Título</h2>
     <figure>
         <img src="./_images/oitava-300.jpg" alt="" />
@@ -56,7 +116,7 @@
         <figcaption>
             Austro securae derecti quicquam toto plagae mare praecipites feras.
         </figcaption>
-    </figure>
+    </figure> -->
 </main>
 
 <?php
