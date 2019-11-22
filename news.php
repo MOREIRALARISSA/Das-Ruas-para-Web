e12e98b6af9cce060d94958930d6b5bd90a60a27
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-11-22 18:11:56 -0300

    news

--- news.php
+++ news.php
@@ -3,30 +3,102 @@
     //Incluido o menu de navegação
 ?>
 <main class="mt-4 mb-4">
-    <div class="row justify-content-center">
-        <h1>Título</h1>
-    </div>
 
-    <div class="row justify-content-center">
-        <div class="col-lg-4 offset-lg-1 col-md-4">
+<div>
 <!-- Botão para acionar modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ExemploModalCentralizado">
-  
-Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalLongoExemplo">
+<figure>
+        <img class="img-fluid" src="./_images/news/plantio.jpg" alt="" />
+        <figcaption>
+        Grafite e plantio de árvores marcam ação integrada no São Camilo
+        </figcaption>
+    </figure>
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+<div class="modal fade" id="ModalLongoExemplo" tabindex="-1" role="dialog" aria-labelledby="TituloModalLongoExemplo" aria-hidden="true">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="TituloModalCentralizado"> News</h5>
+        <h5 class="modal-title" id="TituloModalLongoExemplo">Título do modal</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
+        
+      <h1>Grafite e plantio de árvores marcam ação integrada no São Camilo </h1>
+      <figure>
+        <img class="img-fluid" src="./_images/news/plantio.jpg" alt="" />
+        <figcaption>
+        Cinco grafiteiros do Coletivo The King´s participaram da ação.
+        </figcaption>
+    </figure>
+        <div class=" mt-4 mb-4">
 
+       <p>
+       Um lindo dia de sol marcou a ação integrada realizada pela Fundação Municipal de Ação Social (FUMAS) e Unidades de Gestão de Educação (UGE) e Infraestrutura e Serviços Públicos (UGISP) no Jardim São Camilo, na manhã desta quinta (21): a grafitagem do novo viaduto da Rua João Pinheiro dos Santos e o plantio de árvores no platô ao lado da Viela Sebastião Carneiro Coutinho (Viela do Pelé).
+       </p>
+       <p>
+       Cinco grafiteiros integrantes do Coletivo The King´s – que há duas semanas realizou uma oficina de hip-hop para as crianças assistidas pelo Programa de Suplementação Alimentar (PSA) e também para os alunos da Associação Almater – foram os responsáveis pelos desenhos no viaduto. “Na oficina, pedimos que elas desenhassem o que gostariam de ver aqui no bairro onde moram, então, fizemos uma junção dos desenhos e percebemos que haviam muitos assuntos similares, como flores, parques, brinquedos e muita cor”, destacou Jociel Dune, integrante do Coletivo.
+       </p>
+       <p>
+       Os grafiteiros Ócio, Jen-Jay King, Trezzy, Guskuma e Chiesa passaram para o viaduto algumas das ideias das crianças, além de também preencher os espaços com ilustrações que marcam seus estilos próprios. “O viaduto é a última parte da obra viária realizada pela FUMAS no Jardim São Camilo, e que será entregue à população em dezembro, junto com as ações do aniversário da cidade”, destacou a superintendente da FUMAS, Solange Marques. “O grafite no viaduto se complementa às ilustrações feitas no ano passado no muro que se estende por toda a rua nova que agora interliga todo o bairro. É uma cultura presente na comunidade e que também contribuirá para a melhor preservação do espaço”, acrescentou.
+       </p>
+       <p>
+       <h2> Ipês e azaleias </h2>
+       <p>
+       A animação das crianças era tanta que cada árvore foi batizada com um nome diferente: Anil, Alegrinha, Tirulipa, Princesa, Amizade, Floral e tantos outros foram escolhidos pelas crianças para nomear os 20 ipês que, junto com 150 mudas de azaleias, foram plantados na área livre do platô ao lado do muro de contenção construído para dar sustentação à nova rua do bairro.
+       </p>
+       <p>
+       “As árvores precisam ser bem cuidadas e vocês também são responsáveis por zelar e proteger este espaço”, explicou às crianças o funcionário da Unidade de Desenvolvimento Ambiental (UNIDAN), José Cova, revelando que para deixar o espaço bem colorido, foram escolhidos cinco tipos de ipês: roxo, amarelo, branco, rosa e verde.
+       </p>
+       <p>
+       Para as crianças da Almater, que fizeram os desenhos para inspirar o grafite do viaduto, a ansiedade maior é ver o resultado final do trabalho dos The King´s. “Gostaram muito de participar e estão na maior expectativa para ver a ideia deles na parede”, disse Elaine Cavalcante, coordenadora do Serviço de Convivência da Almater.
+       </p>
+       <p>
+       Os alunos da Deodato também chegaram muito ansiosos para participar do plantio. “Isso tudo é o sentimento de pertencimento, o que contribui para que zelem por este espaço. Daqui a muitos anos, passarão por aqui e terão a lembrança de ter participado do plantio destas árvores”, disse Walkiria Plaza Nunes, coordenadora do Núcleo de Educação Socioambiental da UGE.
+       </p>
+       <p>
+       As amigas Lara, Juliana, Ludmila e Nayara estavam tão animadas que resolveram batizar a árvore com o sentimento que mais desejam que predomine no bairro onde moram. “Nossa árvore se chama Paz, porque no futuro, quando viermos descansar embaixo da sombra dela, queremos olhar tudo e sentir que realmente estamos em paz”, justificou Juliana.
+       </p>
+
+        <p>
+        Fonte <a href="https://tvtecjundiai.com.br/news/2019/11/22/grafite-e-plantio-de-arvores-marcam-acao-integrada-no-sao-camilo/"> TVTEC NEWS | Publicada em 22/11/2019 às 09:55</a> 
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Salvar mudanças</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div>
+<!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalLongoExemplo">
+<figure>
+        <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
+        <figcaption>
+        Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra
+        </figcaption>
+    </figure>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="ModalLongoExemplo" tabindex="-1" role="dialog" aria-labelledby="TituloModalLongoExemplo" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="TituloModalLongoExemplo">News</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
       <h1>Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra </h1>
       <figure>
         <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
@@ -52,15 +124,11 @@ Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serr
         O projeto foi idealizado pelo artista de rua Ramar Gama e pelo produtor cultural Ivan Neves. A eles, se juntaram o também artista de rua João Marcelo Goma (conhecido como Goma) e os produtores culturais Daniel Moreira, da ForSócios; e Gabriel Camargos, João Pedro Illanes e Pedro Carias, da festa Sinestésica.
         </p>
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/DjuCBJNqpWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="450" height="215" src="https://www.youtube.com/embed/DjuCBJNqpWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
         <p>
         Fonte <a href="https://www.hojeemdia.com.br/museu-de-rua-inaugura-interven%C3%A7%C3%B5es-em-bh-ao-grafitar-muro-de-70-metros-na-serra-1.611203"> Jornal Hoje em Dia | 06/04/2018 - 06h00 - Atualizado 12h43 </a> 
         </p>
-
-
-
-    </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
@@ -68,59 +136,9 @@ Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serr
     </div>
   </div>
 </div>
+</div>
+</div>
 
-
-
-
-
-
-
-
-
-
-
-    <!--        <h2>Sub Título</h2>
-            <p>Austro securae derecti quicquam toto plagae mare praecipites feras.</p>
-        </div>
-        <div class="col-lg-4 offset-lg-1 col-md-4">
-            <h2>Sub Título</h2>
-            <p>Austro securae derecti quicquam toto plagae mare praecipites feras.</p>
-        </div>
-    </div>
-
-    <h2>Museu de Rua </h2>
-    
-    <h2>Sub Título</h2>
-    <figure>
-        <img src="./_images/oitava-300.jpg" alt="" />
-        <figcaption>
-            Austro securae derecti quicquam toto plagae mare praecipites feras.
-        </figcaption>
-    </figure>
-
-    <h2>Sub Título</h2>
-    <figure>
-        <img src="./_images/oitava-300.jpg" alt="" />
-        <figcaption>
-            Austro securae derecti quicquam toto plagae mare praecipites feras.
-        </figcaption>
-    </figure>
-
-    <h2>Sub Título</h2>
-    <figure>
-        <img src="./_images/oitava-300.jpg" alt="" />
-        <figcaption>
-            Austro securae derecti quicquam toto plagae mare praecipites feras.
-        </figcaption>
-    </figure>
-
-    <h2>Sub Título</h2>
-    <figure>
-        <img src="./_images/oitava-300.jpg" alt="" />
-        <figcaption>
-            Austro securae derecti quicquam toto plagae mare praecipites feras.
-        </figcaption>
-    </figure> -->
 </main>
 
 <?php
