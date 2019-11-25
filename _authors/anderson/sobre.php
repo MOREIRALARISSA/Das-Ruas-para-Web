85dde6d05bc87a1c32471f785c33a6f665669c84
unknown <andersonlantunes@gmail.com>
2019-11-25 19:46:32 -0300

    Alterado as cores do botão do sub menu dropdown

--- _authors/anderson/sobre.php
+++ _authors/anderson/sobre.php
@@ -16,23 +16,26 @@
 
         </div>
 
+
         <div class="col-lg-4 offset-lg-0 col-md-8 offset-md-2 order-md-first order-sm-first">
-            <p>
-            O <em>Throw-Up</em> é uma pichação evoluída, segundo os grafiteiros. A maior parte das produções de grafite são no estilo <em>Throw-Up</em> 
-            ou conhecidos como <em>BOMB’s</em> por serem mais fáceis, econômicos, e geralmente feitos em lugares não autorizados.
-            </p>
-            <p>
-            São usadas poucas cores, mas bastante contraste entre si. Normalmente são se pinta o fundo e muitas vezes as letras desenhadas têm formato 
-            arredondado, como se tivesse vida própria.
-            </p>
-            <p>
-            Esse estilo foi a fase inicial que levou o ao desenvolvimento do estilo <em>Bubble</em>, em que as letras são redondas, circulares, e muitas vezes 
-            se sobrepõem parcialmente, criando uma imagem que parece expandir-se e formar bolhas.
-            </p>
-            <p>
-            O estilo <em>Bubble</em> pode ser feito em duas cores, onde as letras são pulverizadas em uma cor e posteriormente delineadas com outra, criando um contraste, 
-            ou várias cores podem ser aplicadas para um efeito mais exuberante.
-            </p>
+            <article>
+                <p>
+                O <em>Throw-Up</em> é uma pichação evoluída, segundo os grafiteiros. A maior parte das produções de grafite são no estilo <em>Throw-Up</em> 
+                ou conhecidos como <em>BOMB’s</em> por serem mais fáceis, econômicos, e geralmente feitos em lugares não autorizados.
+                </p>
+                <p>
+                São usadas poucas cores, mas bastante contraste entre si. Normalmente são se pinta o fundo e muitas vezes as letras desenhadas têm formato 
+                arredondado, como se tivesse vida própria.
+                </p>
+                <p>
+                Esse estilo foi a fase inicial que levou o ao desenvolvimento do estilo <em>Bubble</em>, em que as letras são redondas, circulares, e muitas vezes 
+                se sobrepõem parcialmente, criando uma imagem que parece expandir-se e formar bolhas.
+                </p>
+                <p>
+                O estilo <em>Bubble</em> pode ser feito em duas cores, onde as letras são pulverizadas em uma cor e posteriormente delineadas com outra, criando um contraste, 
+                ou várias cores podem ser aplicadas para um efeito mais exuberante.
+                </p>
+            </article>
         </div>
 
         <div class="col-lg-4 offset-lg-0 col-md-5 offset-md-0 col-sm-5 offset-sm-0">
