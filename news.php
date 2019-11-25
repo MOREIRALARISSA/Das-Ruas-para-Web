e921636b039689a8931d7d2b76d3d0b15b1cab96
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-11-25 18:53:23 -0300

    News
    * modal
    * conteúdo
    * imagens

--- news.php
+++ news.php
@@ -3,32 +3,90 @@
     //Incluido o menu de navegação
 ?>
 <main class="mt-4 mb-4">
+   <div class="modal1"> 
+    
+    <div class="card-body">
+     <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
+    <p>
+    Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra
+    </p>
+    <a href="#" class="card-link" data-toggle="modal" data-target="#modalnews1">Saiba mais</a>
+                       
+    </div>
+       <div class="modal fade" id="modalnews1" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">News</h4>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            
+            <div class="modal-body text-center">
+            <h1>Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra </h1>
+                            <figure>
+                                <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
+                                <figcaption>
+                                    Foto Fauno Cultural/Divulgação.
+                                </figcaption>
+                            </figure>
+                            <div class=" mt-4 mb-4">
 
-    <div>
-        <div>
-            <!-- Botão para acionar modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalLongoExemplo">
-                <figure>
-                    <img class="img-fluid" src="./_images/news/plantio.jpg" alt="" />
-                    <figcaption>
-                        Grafite e plantio de árvores marcam ação integrada no São Camilo
-                    </figcaption>
-                </figure>
-            </button>
+                                <p>
+                                    Uma iniciativa inédita de um coletivo de produtores culturais e artistas irá transformar ruas, praças e avenidas de Belo Horizonte em um galeria de arte a céu aberto. Neste sábado (7), das 10h às 19h, será a estreia do Museu de Rua, que pretende converter espaços públicos degradados em imensos murais, locais de contemplação, ocupação e convivência. A primeira ação será realizada na rua Américo Scott, entre as ruas Edgar Coelho e Monte Alegre, no bairro Serra (confira aqui a página do evento).
+                                </p>
+                                <p>
+                                    Os 70 metros de comprimento do muro serão preenchidos simultaneamente com tinta spray a partir dos traços de 20 artistas. Dentre eles estão Goma, Clara Valente, Gud Assis, Ataíde Miranda, Testa e Carol. Além da pintura do muro, evento também terá barraquinhas com música boa e feirinha de produtores locais, como artesanato, roupas (o evento tem uma parceria com a Ancestral para fornecer blusas), bancada de flores, comidas naturais e exposição de arte.
+                                </p>
+                                <p>
+                                    A proposta é manter uma agenda constante de intervenções na cidade. O segundo evento já está marcado para o dia 21 de abril, também na Serra, desta vez na Praça Violeta Soter. Na sequência, a ideia é levar a ação para outras regiões da cidade. Para isso, o Museu de Rua lançou um financiamento coletivo (acesse aqui) para levantar recursos.
+                                </p>
+                                <p>
+                                    Os valores dos apoios vão de R$ 10 até R$ 5 mil. Como recompensa, há os mais variados produtos e serviços, como agradecimentos públicos no dia do evento, CDs de bandas independentes, garrafa da bebida Xeque-Mate, bolsa unisex da Real Grapixo, livro "A Cultura do Grafite. Por um direito das Ruas", de Mariana Gontijo, camisas, lata de spray grafitada por Ramar Gama, bonés, ingressos na Festa Sinestésica, aulas de Yoga, oficina de Graffiti, discotecagem, anuidade do plano de benefícios da empresa ForSócios, telas artísticas e murais.
+                                </p>
+                                <p>
+                                    O projeto foi idealizado pelo artista de rua Ramar Gama e pelo produtor cultural Ivan Neves. A eles, se juntaram o também artista de rua João Marcelo Goma (conhecido como Goma) e os produtores culturais Daniel Moreira, da ForSócios; e Gabriel Camargos, João Pedro Illanes e Pedro Carias, da festa Sinestésica.
+                                </p>
 
-            <!-- Modal -->
-            <div class="modal fade" id="ModalLongoExemplo" tabindex="-1" role="dialog" aria-labelledby="TituloModalLongoExemplo" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="TituloModalLongoExemplo">Título do modal</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
+                                <iframe width="450" height="215" src="https://www.youtube.com/embed/DjuCBJNqpWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-                            <h1>Grafite e plantio de árvores marcam ação integrada no São Camilo </h1>
+                                <p>
+                                    Fonte <a href="https://www.hojeemdia.com.br/museu-de-rua-inaugura-interven%C3%A7%C3%B5es-em-bh-ao-grafitar-muro-de-70-metros-na-serra-1.611203"> Jornal Hoje em Dia | 06/04/2018 - 06h00 - Atualizado 12h43 </a>
+                                </p>
+                            </div>
+            </div>
+            <div class="modal-footer">
+            <button type="button"class="btn btn-danger" data-dismiss="modal">Fechar</button>
+            </div>
+            
+        </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal2">
+<div class="card-body">
+     <img class="img-fluid" src="./_images/news/plantio.jpg" alt="" />
+    <p>
+    Grafite e plantio de árvores marcam ação integrada no São Camilo 
+    </p>
+    <a href="#" class="card-link" data-toggle="modal" data-target="#modalnews2">Saiba mais</a>
+                       
+    </div>
+       <div class="modal fade" id="modalnews2" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            
+            <div class="modal-header">
+                <h4 class="modal-title">News</h4>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            
+            <div class="modal-body text-center-justify">
+                        <h1>Grafite e plantio de árvores marcam ação integrada no São Camilo </h1>
                             <figure>
                                 <img class="img-fluid" src="./_images/news/plantio.jpg" alt="" />
                                 <figcaption>
@@ -68,80 +126,18 @@
                                         Fonte <a href="https://tvtecjundiai.com.br/news/2019/11/22/grafite-e-plantio-de-arvores-marcam-acao-integrada-no-sao-camilo/"> TVTEC NEWS | Publicada em 22/11/2019 às 09:55</a>
                                     </p>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                                <button type="button" class="btn btn-primary">Salvar mudanças</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </div>
-
-        <div>
-            <!-- Botão para acionar modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalLongoExemplo">
-                <figure>
-                    <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
-                    <figcaption>
-                        Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra
-                    </figcaption>
-                </figure>
-            </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="ModalLongoExemplo" tabindex="-1" role="dialog" aria-labelledby="TituloModalLongoExemplo" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="TituloModalLongoExemplo">News</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-
-                        <div class="modal-body">
-                            <h1>Museu de Rua inaugura intervenções em BH ao grafitar muro de 70 metros na Serra </h1>
-                            <figure>
-                                <img class="img-fluid" src="./_images/news/museuderua.jpg" alt="" />
-                                <figcaption>
-                                    Foto Fauno Cultural/Divulgação.
-                                </figcaption>
-                            </figure>
-                            <div class=" mt-4 mb-4">
-
-                                <p>
-                                    Uma iniciativa inédita de um coletivo de produtores culturais e artistas irá transformar ruas, praças e avenidas de Belo Horizonte em um galeria de arte a céu aberto. Neste sábado (7), das 10h às 19h, será a estreia do Museu de Rua, que pretende converter espaços públicos degradados em imensos murais, locais de contemplação, ocupação e convivência. A primeira ação será realizada na rua Américo Scott, entre as ruas Edgar Coelho e Monte Alegre, no bairro Serra (confira aqui a página do evento).
-                                </p>
-                                <p>
-                                    Os 70 metros de comprimento do muro serão preenchidos simultaneamente com tinta spray a partir dos traços de 20 artistas. Dentre eles estão Goma, Clara Valente, Gud Assis, Ataíde Miranda, Testa e Carol. Além da pintura do muro, evento também terá barraquinhas com música boa e feirinha de produtores locais, como artesanato, roupas (o evento tem uma parceria com a Ancestral para fornecer blusas), bancada de flores, comidas naturais e exposição de arte.
-                                </p>
-                                <p>
-                                    A proposta é manter uma agenda constante de intervenções na cidade. O segundo evento já está marcado para o dia 21 de abril, também na Serra, desta vez na Praça Violeta Soter. Na sequência, a ideia é levar a ação para outras regiões da cidade. Para isso, o Museu de Rua lançou um financiamento coletivo (acesse aqui) para levantar recursos.
-                                </p>
-                                <p>
-                                    Os valores dos apoios vão de R$ 10 até R$ 5 mil. Como recompensa, há os mais variados produtos e serviços, como agradecimentos públicos no dia do evento, CDs de bandas independentes, garrafa da bebida Xeque-Mate, bolsa unisex da Real Grapixo, livro "A Cultura do Grafite. Por um direito das Ruas", de Mariana Gontijo, camisas, lata de spray grafitada por Ramar Gama, bonés, ingressos na Festa Sinestésica, aulas de Yoga, oficina de Graffiti, discotecagem, anuidade do plano de benefícios da empresa ForSócios, telas artísticas e murais.
-                                </p>
-                                <p>
-                                    O projeto foi idealizado pelo artista de rua Ramar Gama e pelo produtor cultural Ivan Neves. A eles, se juntaram o também artista de rua João Marcelo Goma (conhecido como Goma) e os produtores culturais Daniel Moreira, da ForSócios; e Gabriel Camargos, João Pedro Illanes e Pedro Carias, da festa Sinestésica.
-                                </p>
-
-                                <iframe width="450" height="215" src="https://www.youtube.com/embed/DjuCBJNqpWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-                                <p>
-                                    Fonte <a href="https://www.hojeemdia.com.br/museu-de-rua-inaugura-interven%C3%A7%C3%B5es-em-bh-ao-grafitar-muro-de-70-metros-na-serra-1.611203"> Jornal Hoje em Dia | 06/04/2018 - 06h00 - Atualizado 12h43 </a>
-                                </p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="modal-footer">
+            <button type="button"class="btn btn-danger" data-dismiss="modal">Fechar</button>
             </div>
+            
         </div>
-
+        </div>
     </div>
+
+
+
+</div>
 </main>
 
 <?php
