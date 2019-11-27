7dbdc8a1ab7fd85f25a6c79d5e76a0a1b8a2dd6c
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-11-27 15:11:01 -0300

    News, Footer, Grafite pleo Mundo e style.css

--- grafite-no-mundo.php
+++ grafite-no-mundo.php
@@ -10,8 +10,16 @@
 
             <div class="col-12 text-center mt-3">
 
-                <h1 class="display-3">Grafite no mundo</h1>
-                <p></p>
+                <h1 class="display-3 mt-4 mb-4">Grafite no mundo</h1>
+
+                <p class="text-left">
+                 O grafite é uma verdadeira obra de arte, capaz de modificar lugares – sem falar que são formas de manifestações artísticas em espaços públicos. Aqui no Brasil, existem grandes nomes que espalham cores e desenhos pelas paredes e calçadas do país, sendo a cidade de São Paulo um dos lugares preferidos para se observar esse tipo de arte.
+
+                </p>
+                <p class="text-left">
+                Ao redor do mundo é possível observar grandes artistas que levam a sério a arte de rua e transformam “lugares normais” em verdadeiras telas. Pensando nisso, <a href="guiadasemana.com.br/viagens-internacionais/galeria/grafites-incriveis-ao-redor-do-mundo"> o Guia da Semana</a>  fez uma lista 10 grafites incríveis ao redor do mundo que você precisa conhecer. Confira:
+                </p>
+
 
             </div>
 
@@ -25,7 +33,7 @@
 
                     <nav class="nav nav-pills flex-column">
 
-                        <a class="nav-link active" href="#item1">Brasil</a>
+                        <a class="nav-link active" href="#item1">Kobra (BR) </a>
                         <!--
                         <nav class="nav nav-pills flex-column">
 
@@ -50,32 +58,25 @@
 
                 <div data-spy="scroll" data-target="#navbarVertical" data-offset="0" class="scrollspySite ">
 
-                    <h3 id="item1">Brasil</h3>
+            
+
+                    <h3 id="item1">Kobra</h3>
 
                     <p>
-                        Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
-                        madescit umor aere conversa undas tegi sanctius boreas quoque diverso iuga retinebat zonae numero umor animal
-                        primaque locis elementaque terris arce auroram lanient tonitrua sublime aquae campos conversa phoebe rudis primaque
-                        invasit ligavit: cognati umentia verba invasit obstabatque imagine animal utque quam exemit natus mentisque surgere
-                        fratrum invasit satus quinta erat: ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-                        erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-                        timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia habentem obsistitur permisit motura
-                        ventos ad manebat porrexerat nullo moles iunctarum longo animal ventos piscibus vix arce nix ardentior effigiem cingebant
-                        cinxit scythiam coeptis semine liquidas peregrinum pendebat reparabat recens nubes extendi elementaque praeter duas eurus
-                        viseret subdita frigida quem praeter longo locis arce spectent.
+                    Um dos artistas brasileiros mais conhecidos quando o assunto é arte de rua é o Eduardo Kobra. Espalhados por arranha-céus e pontos bem inusitados, os murais super coloridos do artista chamam a atenção de qualquer um. Talvez um dos trabalhos mais famosos do brasileiro seja "O Beijo", em Nova York, que retrata a famosa cena em que um marinheiro e uma enfermeira se beijam para celebrar o final da Segunda Guerra Mundial.
                     </p>
                     
                     <div class="row justify-content-center mx-auto">
                         <div class="col-lg-12">
                             <figure>
-                                <img class="img-fluid" src="./_images/oitava-300.jpg" alt="" />
-                                <h3>Sub Título 2</h3>
-                                <figcaption class="text-justify">
-                                    ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-                                    erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-                                    timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia.
-                                </figcaption>
+                                <img class="img-fluid" src="./_images/GrafitePeloMundo/kobra.jpg" alt="" />
+                                                               <figcaption class="text-justify">
+                                    Foto Cristina Mueller
+                </figcaption>
                             </figure>
+                            <p>
+                                    Chelsea é um antigo bairro industrial de Nova York que ganhou contornos boêmios, com galerias, bares, restaurantes e o High Line! Alias, você pode esbarrar em um grafite – “o Beijo” – do artista brasileiro Kobra.
+                </p>
                         </div>
                     </div>
 
