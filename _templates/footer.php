f98c45d52f09a11e89e86bdfb10527a3462380b9
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-11-27 21:27:31 -0300

    Update footer.php

--- _templates/footer.php
+++ _templates/footer.php
@@ -47,7 +47,10 @@
         </div>
 
     </div>
-
+    <div>
+    
+    <p><small>Copyright 2019 | ᴅᴀs ʀᴜᴀs ᴘᴀʀᴀ ᴡᴇʙ </small></p>
+</div>
 </footer>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
