e551c366b0cb4fbb824264b61c1f3efc176d2ecf
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-11-30 19:35:44 -0300

    Estilos teste

--- estilos.php
+++ estilos.php
@@ -10,7 +10,7 @@
     <div class="card-deck">
     
         <div class="card">
-            <img src="_images/larissa/1.jpg" class="card-img-top" alt="...">
+            <img src="_images/estilos/freestyle.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <a href="_authors/igor/index.php"><h5 class="card-title">Freestyle</h5></a>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -18,7 +18,7 @@
         </div>
         
         <div class="card">
-            <img src="_images/larissa/1.jpg" class="card-img-top" alt="...">
+            <img src="_images/estilos/throwup.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <a href="_authors/anderson/index.php"><h5 class="card-title">Throwup</h5></a>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -27,14 +27,14 @@
             
  
         <div class="card">
-            <img src="_images/larissa/1.jpg" class="card-img-top" alt="...">
+            <img src="_images/estilos/stencil.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <a href="_authors/matheus/index.php"><h5 class="card-title">Stencil</h5></a>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
             </div>
         </div>
         <div class="card">
-            <img src="_images/larissa/1.jpg" class="card-img-top" alt="...">
+            <img src="_images/estilos/mural.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <a href="_authors/larissa/index.php"><h5 class="card-title">Mural</h5></a>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -42,7 +42,7 @@
         </div>
   
         <div class="card">
-            <img src="_images/larissa/1.jpg" class="card-img-top" alt="...">
+            <img src="_images/estilos/3d.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <a  href="_authors/adelina/index.php"><h5 class="card-title">3D</h5></a>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
