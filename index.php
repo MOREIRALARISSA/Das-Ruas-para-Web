292b41bf87c6890584750e077152c3942973b85f
unknown <andersonlantunes@gmail.com>
2019-11-30 16:47:56 -0300

    Alterado estrutura da index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         
             <div class="carousel-item active">
             
-                <img src="_images/GrafitexSociedade/hellsKit1970s.jpg" class="d-block w-100">
+                <img src="_images/GrafitexSociedade/hellsKit1970s.jpg" class="img-fluid d-block">
                 <div class="carousel-caption d-none d-md-block">
                 
                     <h3></h3>
@@ -30,7 +30,7 @@
             
             <div class="carousel-item">
             
-                <img src="_images/HistoriaDoGrafite/NewYork1980.jpg" class="d-block w-100">
+                <img src="_images/HistoriaDoGrafite/NewYork1980.jpg" class="img-fluid d-block">
                 <div class="carousel-caption d-none d-md-block">
                 
                     <h3></h3>
@@ -42,7 +42,7 @@
             
             <div class="carousel-item">
             
-                <img src="_images/GrafitexSociedade/hellsKit1970s.jpg" class="d-block w-100">
+                <img src="_images/GrafitexSociedade/hellsKit1970s.jpg" class="img-fluid d-block">
                 <div class="carousel-caption d-none d-md-block">
                 
                     <h3></h3>
@@ -56,14 +56,14 @@
         
         <a class="carousel-control-prev" href="#carouselSite" role="button" data-slide="prev">
         
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="carousel-control-prev-icon"></span>
             <span class="sr-only">Anterior</span>
         
         </a>
     
-        <a class="carousel-control-next" href="#carouselSite" role="button" data-slide="next">
+        <a class="carousel-control-next" id="#carouselSite" role="button" data-slide="next">
         
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="carousel-control-next-icon"></span>
             <span class="sr-only">Avançar</span>
             
         </a>
@@ -72,90 +72,137 @@
     
     <div class="container">
 
-    <div class="row justify-content-center">
-        <h1>Título</h1>
-    </div>
-
-    <div class="text-justify">
-        <p>
-            Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
-            madescit umor aere conversa undas tegi sanctius boreas quoque diverso iuga retinebat zonae numero umor animal
-            primaque locis elementaque terris arce auroram lanient tonitrua sublime aquae campos conversa phoebe rudis primaque
-            invasit ligavit: cognati umentia verba invasit obstabatque imagine animal utque quam exemit natus mentisque surgere
-            fratrum invasit satus quinta erat: ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-            erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-            timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia habentem obsistitur permisit motura
-            ventos ad manebat porrexerat nullo moles iunctarum longo animal ventos piscibus vix arce nix ardentior effigiem cingebant
-            cinxit scythiam coeptis semine liquidas peregrinum pendebat reparabat recens nubes extendi elementaque praeter duas eurus
-            viseret subdita frigida quem praeter longo locis arce spectent.
-        </p>
-    </div>
-
-    <div>
-        <h2>Sub Título</h2>
-    </div>
-
-    <div class="row justify-content-center">
-        <div class="col-lg-12">
-            <figure>
-                <img class="img-fluid" src="./_images/oitava-300.jpg" alt="" />
-                <h3>Sub Título 2</h3>
-                <figcaption class="text-justify">
-                    ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-                    erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-                    timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia.
-                </figcaption>
-            </figure>
+        <div class="row justify-content-center">
+            <h1>Título</h1>
         </div>
-    </div>
 
-    <h2>Sub Título</h2>
+        <div class="text-justify">
+            <p>
+                Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                madescit umor aere conversa undas tegi sanctius boreas quoque diverso iuga retinebat zonae numero umor animal
+                primaque locis elementaque terris arce auroram lanient tonitrua sublime aquae campos conversa phoebe rudis primaque
+                invasit ligavit: cognati umentia verba invasit obstabatque imagine animal utque quam exemit natus mentisque surgere
+                fratrum invasit satus quinta erat: ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
+                erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
+                timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia habentem obsistitur permisit motura
+                ventos ad manebat porrexerat nullo moles iunctarum longo animal ventos piscibus vix arce nix ardentior effigiem cingebant
+                cinxit scythiam coeptis semine liquidas peregrinum pendebat reparabat recens nubes extendi elementaque praeter duas eurus
+                viseret subdita frigida quem praeter longo locis arce spectent.
+            </p>
+        </div>
 
-    <figure>
-        <img class="img-fluid" src="./_images/oitava-300.jpg" alt="" />
-        <h3>Sub Título 2</h3>
-        <figcaption class="text-justify">
-            ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-            erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-            timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia.
-        </figcaption>
-    </figure>
+        <div class="row">
+            <div class="col-4">
+                <h2>Título</h2>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi sanctius boreas quoque diverso iuga retinebat zonae numero umor animal
+                    primaque locis elementaque terris arce auroram lanientventos aethera ardentior frigida uno timebat aeris quae 
+                    caelum aethere pendebat erant oppida terrenae porrexerat siccis peragebant</p>
+            </div>
+            <div class="col-4">
+                <h2>Título</h2>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+                <h2>Título</h2>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+            </div>
+            <div class="col-4">
+                <h2>Título</h2>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+                <h2>Título</h2>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Texto temporário</strong>
+                    </figcaption>
+                </figure>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Texto temporário</strong>
+                    </figcaption>
+                </figure>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Texto temporário</strong>
+                    </figcaption>
+                </figure>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Texto temporário</strong>
+                    </figcaption>
+                </figure>
+                <p>Austro securae derecti quicquam toto plagae mare praecipites feras, super orba quarum radiis inclusum mortales
+                    madescit umor aere conversa undas tegi</p>
+            </div>
+        </div>
 
-    <h2>Sub Título</h2>
-
-    <figure>
-        <img class="img-fluid" src="./_images/oitava-300.jpg" alt="" />
-        <h3>Sub Título 2</h3>
-        <figcaption class="text-justify">
-            ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-            erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-            timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia.
-        </figcaption>
-    </figure>
-
-    <h2>Sub Título</h2>
-
-    <figure>
-        <img class="img-fluid" src="./_images/oitava-300.jpg" alt="" />
-        <h3>Sub Título 2</h3>
-        <figcaption class="text-justify">
-            ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-            erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-            timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia.
-        </figcaption>
-    </figure>
-
-    <h2>Sub Título</h2>
-
-    <figure>
-        <img class="img-fluid" src="./_images/oitava-300.jpg" alt="" />
-        <h3>Sub Título 2</h3>
-        <figcaption class="text-justify">
-            ventos aethera ardentior frigida uno timebat aeris quae caelum aethere pendebat
-            erant oppida terrenae porrexerat siccis peragebant pugnabant quae eurus melior semine congestaque deorum solum vindice
-            timebat convexi emicuit cum vis viseret et deducite os capacius inposuit aestu omnia.
-        </figcaption>
-    </figure>
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Algo sobre o estilo</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Algo sobre o estilo</strong>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-4">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Algo sobre o estilo</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Algo sobre o estilo</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <figure>
+                    <img src="./_images/anderson/artista-1.jpg" alt="" />
+                    <figcaption>
+                        <strong>Algo sobre o estilo</strong>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
 
     </div>
         
