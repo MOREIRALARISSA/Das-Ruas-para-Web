8a5c7d1389e966dcf9ff5b61d130078edfb3ece2
unknown <andersonlantunes@gmail.com>
2019-12-01 19:24:15 -0300

    Acertado semnântica da página Throwup

--- _authors/anderson/index.php
+++ _authors/anderson/index.php
@@ -28,45 +28,43 @@
                             <a class="nav-link " id="nav-pills-03" data-toggle="pill" href="#nav-item-03">Galeria</a>
                         </li>
                     </ul>
-                    <div class="tab-content" id="nav-pills-content">
+
+                    <!-- Sobre -->
+                    <div class="tab-content" id="nav-pills-content">                        
                         <div class="tab-pane fade show active" id="nav-item-01" role="tabpanel">
-                            <!-- Sobre -->
                             <h1 class="mt-4 mb-4"> SOBRE </h1>
-                            <div class="row align-items-center">
-                                <aside>
+                            <article>
+                                <div class="row align-items-center">
                                     <div class="col-lg-4 offset-lg-0 order-lg-first col-md-5 offset-md-1 col-sm-6 offset-sm-0 pl-0 pr-0 col-6 text-center">
                                         <img class="img-fluid" src="./../../_images/anderson/sobre-1.jpg" alt="Parte esquerda de um grafite dividido em dois" />
                                     </div>
-                                </aside>
 
-                                <article>
-                                    <h2>Sobre</h2>
                                     <div class="col-lg-4 offset-lg-0 col-md-8 offset-md-2 order-md-first order-sm-first order-first">
-                                        <p>
-                                            O <em>Throw-Up</em> é uma pichação evoluída, segundo os grafiteiros. A maior parte das produções de grafite são no estilo <em>Throw-Up</em>
-                                            ou conhecidos como <em>BOMB’s</em> por serem mais fáceis, econômicos, e geralmente feitos em lugares não autorizados.
-                                        </p>
-                                        <p>
-                                            São usadas poucas cores, mas bastante contraste entre si. Normalmente são se pinta o fundo e muitas vezes as letras desenhadas têm formato
-                                            arredondado, como se tivesse vida própria.
-                                        </p>
-                                        <p>
-                                            Esse estilo foi a fase inicial que levou o ao desenvolvimento do estilo <em>Bubble</em>, em que as letras são redondas, circulares, e muitas vezes
-                                            se sobrepõem parcialmente, criando uma imagem que parece expandir-se e formar bolhas.
-                                        </p>
-                                        <p>
-                                            O estilo <em>Bubble</em> pode ser feito em duas cores, onde as letras são pulverizadas em uma cor e posteriormente delineadas com outra, criando um contraste,
-                                            ou várias cores podem ser aplicadas para um efeito mais exuberante.
-                                        </p>
-                                    </div>
-                                </article>
 
-                                <aside>
+                                            <h2>Sobre</h2>
+                                            <p>
+                                                O <em>Throw-Up</em> é uma pichação evoluída, segundo os grafiteiros. A maior parte das produções de grafite são no estilo <em>Throw-Up</em>
+                                                ou conhecidos como <em>BOMB’s</em> por serem mais fáceis, econômicos, e geralmente feitos em lugares não autorizados.
+                                            </p>
+                                            <p>
+                                                São usadas poucas cores, mas bastante contraste entre si. Normalmente são se pinta o fundo e muitas vezes as letras desenhadas têm formato
+                                                arredondado, como se tivesse vida própria.
+                                            </p>
+                                            <p>
+                                                Esse estilo foi a fase inicial que levou o ao desenvolvimento do estilo <em>Bubble</em>, em que as letras são redondas, circulares, e muitas vezes
+                                                se sobrepõem parcialmente, criando uma imagem que parece expandir-se e formar bolhas.
+                                            </p>
+                                            <p>
+                                                O estilo <em>Bubble</em> pode ser feito em duas cores, onde as letras são pulverizadas em uma cor e posteriormente delineadas com outra, criando um contraste,
+                                                ou várias cores podem ser aplicadas para um efeito mais exuberante.
+                                            </p>
+                                    </div>
+
                                     <div class="col-lg-4 offset-lg-0 col-md-5 offset-md-0 col-sm-6 offset-sm-0 pl-0 pr-0 col-6 text-center">
                                         <img class="img-fluid" src="./../../_images/anderson/sobre-2.jpg" alt="Parte direita de um grafite dividido em dois" />
                                     </div>
-                                </aside>
-                            </div>
+                                </div>
+                            </article>
                         </div>
                         
                         <!-- Artistas -->
