ea6759d6c6f165b18ea7822451e69b6429fac811
unknown <andersonlantunes@gmail.com>
2019-12-02 18:58:56 -0300

    Página Grafite x Sociedade Validada e alterado estrutura

--- grafite-x-sociedade.php
+++ grafite-x-sociedade.php
@@ -3,67 +3,66 @@
     //Incluido o menu de navegação
 ?>
 <main class="mt-4 mb-4">
-
     <div class="container">
-
-        <div class="row justify-content-center">
-            <h1>Grafite x Sociedade</h1>
-        </div>
-
-        <p>
-            Quem vive nos grandes centros urbanos pode encontrar os grafites com certa frequência. Tomando proporções cada vez maiores, o grafite se impõe nos muros das cidades, dispensando a necessidade de se expor em galerias.
-        </p>
-        <p>
-            O fato é que, ainda que já conte com certo tempo de história — a Humanidade explora a pintura artística em paredes desde a arte rupestre. Contudo, as opiniões divergem, pois há quem acredita que o grafite seja vandalismo e outros o classificam como arte, revelando a necessidade de debatermos esse tema sobre o que é grafite.
-        </p>
-
-        <figure>
-            <img class="img-fluid" src="./_images/GrafitexSociedade/hellsKit1970s.jpg" class="img-fluid" alt=" Mural no Hell's Kitchen Playground nos anos 1970." title="Mural Hell's Kitchen 1970" />
-            <figcaption>
-                Mural no Hell's Kitchen Playground nos anos 1970.
-            </figcaption>
-        </figure>
-
-        <h2>O que é o Grafite ?</h2>
-        <p>
-            O grafite é uma forma de expressão artística que utiliza locais públicos — muros, paredes de grandes edifícios e até o chão — como tela.
-        </p>
-        <p>
-            Tal como o conhecemos, a história do grafite remonta ao final dos anos 1960 e início dos anos 1970 em cidades como Paris e Nova York, locais que começaram a mostrar em suas paredes as primeiras obras de artistas anônimos que buscavam se expressar.
-        </p>
-
-        <figure>
-            <img class="img-fluid" src="./_images/GrafitexSociedade/1sTrainGrafite.jpg" class="img-fluid" alt="O primeiro trem a ser coberto por grafite | New York anos 1970." title="O primeiro trem a ser coberto por grafite" />
-            <figcaption>
-                O primeiro trem a ser coberto por grafite | New York anos 1970.
-            </figcaption>
-        </figure>
-
-        <h2>Grafite no Brasil</h2>
-        <p>
-            No Brasil, a arte de rua surge na década de 70, mais precisamente com as obras de grafite nas paredes da cidade de São Paulo. Curiosamente surgiu numa época conturbada da história do país, com o advento da Ditadura Militar.
-        </p>
-        <p>
-            A central proposição da arte urbana é justamente sair dos lugares ditos “consagrados”, ou seja, destinados à exposição e apresentações artísticas (equipamentos culturais: teatro, cinemas, bibliotecas, museus), para dar visibilidade a arte cotidiana, espalhada pelas ruas.
-        </p>
-        <p>
-
-            Os temas utilizados pelos artistas de rua são bem diversos, no entanto, muitos trabalhos estão pautados em críticas sociais, políticas e econômicas.
-
-            Importante analisar o crescimento da arte urbana nos últimos tempos, de forma que passa a ser vista como um “valor cultural” muito importante das minorias que vivem nos centros urbanos, e anseiam em mostrar sua arte.
-
-            Assim, essas manifestações populares permitem o encontro com a arte independente, apesar de muitos artistas de ruas, terem se consagrado mundialmente, reconhecidos pela mídia, indústria e diversos meios de comunicação em massa era uma arte marginalizada.
-        </p>
-
-        <figure>
-            <img class="img-fluid" src="./_images/GrafitexSociedade/KelburnCastle.jpg" class="img-fluid" alt="Kelburn Castle, 2007" />
-            <figcaption>
-                Kelburn Castle, Escócia - 2007 | Nina Pandolfo, os Gêmeos e Nunca, ambos brasileiros.
-            </figcaption>
-        </figure>
-
+        <article>
+            <div class="row justify-content-center">
+                <h1>Grafite x Sociedade</h1>
+            </div>
+            <div class="row mt-5">
+                <div class="col-lg-6 align-self-center">
+                    <p>
+                        Quem vive nos grandes centros urbanos pode encontrar os grafites com certa frequência. Tomando proporções cada vez maiores, o grafite se impõe nos muros das cidades, dispensando a necessidade de se expor em galerias.
+                    </p>
+                    <p>
+                        O fato é que, ainda que já conte com certo tempo de história — a Humanidade explora a pintura artística em paredes desde a arte rupestre. Contudo, as opiniões divergem, pois há quem acredita que o grafite seja vandalismo e outros o classificam como arte, revelando a necessidade de debatermos esse tema sobre o que é grafite.
+                    </p>
+                </div>
+                <div class="col-lg-5">
+                    <figure>
+                        <img class="img-fluid" src="./_images/GrafitexSociedade/hellsKit1970s.jpg" alt=" Mural no Hell's Kitchen Playground nos anos 1970." title="Mural Hell's Kitchen 1970" />
+                        <figcaption>
+                            Mural no Hell's Kitchen Playground nos anos 1970.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <section class="mt-5">
+                <h2>O que é o Grafite ?</h2>
+                <p>
+                    O grafite é uma forma de expressão artística que utiliza locais públicos — muros, paredes de grandes edifícios e até o chão — como tela.
+                </p>
+                <p>
+                    Tal como o conhecemos, a história do grafite remonta ao final dos anos 1960 e início dos anos 1970 em cidades como Paris e Nova York, locais que começaram a mostrar em suas paredes as primeiras obras de artistas anônimos que buscavam se expressar.
+                </p>
+                <figure class="mt-5">
+                    <img class="img-fluid" src="./_images/GrafitexSociedade/1sTrainGrafite.jpg" alt="O primeiro trem a ser coberto por grafite | New York anos 1970." title="O primeiro trem a ser coberto por grafite" />
+                    <figcaption>
+                        O primeiro trem a ser coberto por grafite | New York anos 1970.
+                    </figcaption>
+                </figure>
+            </section>
+            <section class="mt-5 mb-5">
+                <h2>Grafite no Brasil</h2>
+                <p>
+                    No Brasil, a arte de rua surge na década de 70, mais precisamente com as obras de grafite nas paredes da cidade de São Paulo. Curiosamente surgiu numa época conturbada da história do país, com o advento da Ditadura Militar.
+                </p>
+                <p>
+                    A central proposição da arte urbana é justamente sair dos lugares ditos “consagrados”, ou seja, destinados à exposição e apresentações artísticas (equipamentos culturais: teatro, cinemas, bibliotecas, museus), para dar visibilidade a arte cotidiana, espalhada pelas ruas.
+                </p>
+                <p>
+                    Os temas utilizados pelos artistas de rua são bem diversos, no entanto, muitos trabalhos estão pautados em críticas sociais, políticas e econômicas.
+                    Importante analisar o crescimento da arte urbana nos últimos tempos, de forma que passa a ser vista como um “valor cultural” muito importante das minorias que vivem nos centros urbanos, e anseiam em mostrar sua arte.
+                    Assim, essas manifestações populares permitem o encontro com a arte independente, apesar de muitos artistas de ruas, terem se consagrado mundialmente, reconhecidos pela mídia, indústria e diversos meios de comunicação em massa era uma arte marginalizada.
+                </p>
+                <figure class="mt-5">
+                    <img class="img-fluid" src="./_images/GrafitexSociedade/KelburnCastle.jpg" alt="Kelburn Castle, 2007" />
+                    <figcaption>
+                        Kelburn Castle, Escócia - 2007 | Nina Pandolfo, os Gêmeos e Nunca, ambos brasileiros.
+                    </figcaption>
+                </figure>
+            </section>
+        </article>
     </div>
-
 </main>
 
 <?php
