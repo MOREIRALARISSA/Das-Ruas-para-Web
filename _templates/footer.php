e543f3836b1bb6b53b8ed79256f967b3b96806eb
unknown <andersonlantunes@gmail.com>
2019-12-01 22:19:16 -0300

    Retirado tag <p> para removre margem inferior da página

--- _templates/footer.php
+++ _templates/footer.php
@@ -49,16 +49,13 @@
 
     </div>
     
-    <div class=" row  d-lg-none d-block text-center">
-    <img class="img-fluid" src="<?php echo !$resultado ? "" : "../../" ;?>_images/logotipo_mobile.png" />
+    <div class="row d-lg-none d-block text-center">
+        <img class="img-fluid" src="<?php echo !$resultado ? "" : "../../" ;?>_images/logotipo_mobile.png" />
     </div>
     
-    
-    
     <div class="copy">
-    
-    <p><small>Copyright 2019 | ᴅᴀs ʀᴜᴀs ᴘᴀʀᴀ ᴡᴇʙ </small></p>
-</div>
+        <small>Copyright 2019 | ᴅᴀs ʀᴜᴀs ᴘᴀʀᴀ ᴡᴇʙ </small>
+    </div>
 </footer>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
