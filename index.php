49e59212510aeb896d267e14a216724ead268460
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-12-03 12:48:35 -0300

    teste

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     //Incluindo o menu de navegação
 ?>
 <main class="mt-0 mb-4">
-    <div id="carouselSite" class="carousel slide" data-ride="carousel">
+   
+   
+   <!-- <div id="carouselSite" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselSite" data-slide-to="0" class="active"></li>
             <li data-target="#carouselSite" data-slide-to="1"></li>
@@ -44,10 +46,10 @@
             <span class="carousel-control-next-icon"></span>
             <span class="sr-only">Avançar</span>
         </a>
-    </div>
+    </div> -->
 
     <div class="container">
-        <div class="row justify-content-center">
+         <!--<div class="row justify-content-center">
             <h1>Título</h1>
         </div>
         <div class="text-justify">
@@ -63,35 +65,35 @@
                 cinxit scythiam coeptis semine liquidas peregrinum pendebat reparabat recens nubes extendi elementaque praeter duas eurus
                 viseret subdita frigida quem praeter longo locis arce spectent.
             </p>
-        </div>
+        </div> -->
         <div class="row">
-            <div class="col-4">
+            <div class="col-4 mt-5">
            <h2> <a href="quem-somos.php"> Das Ruas para Web </a></h2>
                 
                 <p class="text-justify">
                 Portal sobre <strong>GRAFITE</strong> desenvolvido pelos estudantes do curso sistemas para internet da FATEC São Roque, o grupo é composto por cinco integrantes: Adelina, Anderson, Igor, Larissa e Matheus.
                 </p>
             </div>
-            <div class="col-4">
+            <div class="col-4 mt-5">
 
                 <div class="col-8">
-                <a href="grafite-x-sociedade.php"> <img src="./_images/grafite-x-sociedade/hellsKit1970s.jpg" class="col-12" alt="..."> </a>
+                <a href="grafite-x-sociedade.php"> <img src="./_images/grafite-x-sociedade/hellsKit1970s.jpg" class="img-rounded col-12" alt="..."> </a>
                 <h2 class="text-center txtimg"><a href="grafite-x-sociedade.php">O que é o Grafite ?</a></h2>
                 </div> 
 
                 <div class="col-8">
-                <a href="historia-do-grafite.php"><img src="./_images/historia-do-grafite/grafite1982.jpg" class="col-12" alt="..."> </a>
+                <a href="historia-do-grafite.php"><img src="./_images/historia-do-grafite/grafite1982.jpg" class="img-rounded col-12" alt="..."> </a>
                 <h2 class="text-center txtimg"><a href="historia-do-grafite.php">História do Grafite</a></h2>
                 </div>                
             </div>
 
-            <div class="col-4">
+            <div class="col-4 mt-5">
                 <div class="col-8">
-                <a href="mitos-e-verdades.php"> <img src="./_images/mitos-e-verdades/pichacao-centro-bh.jpg" class="col-12" alt="..."> </a>
+                <a href="mitos-e-verdades.php"> <img src="./_images/mitos-e-verdades/pichacao-centro-bh.jpg" class="img-rounded col-12" alt="..."> </a>
                 <h2 class="text-center txtimg"><a href="mitos-e-verdades.php">Pichação é crime. Grafite não é.</a></h2>
                 </div>  
                 <div class="col-8">
-                <a href="grafite-no-mundo.php"><img src="./_images/mitos-e-verdades/pichacao-centro-bh.jpg" class="col-12" alt="..."> </a>
+                <a href="grafite-no-mundo.php"><img src="./_images/grafite-no-mundo/kobra.jpg" class="img-rounded col-12" alt="..."> </a>
                 <h2 class="text-center txtimg"><a href="grafite-no-mundo.php">Grafite no Mundo</a></h2>
                 </div>            
             </div>
@@ -144,7 +146,7 @@
         <div class="row justify-content-center">        
             <div class="col-lg-6 col-md-6 mb-4">
                 <div class="card" style="height: 16rem; width: 18rem;">
-                    <a href="_authors/igor/index.php"> <img class="card-img-top rounded" src="_images/estilos/freestyle.png" alt="..."> </a>
+                    <a href="_authors/igor/index.php"> <img class="card-img-top rounded" src="_images/estilos/freestyle.png" alt="Imagem capa Grafite Freestyle"> </a>
                     <div class="card-body">
                     <a href="_authors/igor/index.php"><p class="card-text text-right"><strong>Saiba mais</strong></p>
                     </div>
@@ -152,7 +154,7 @@
             </div>
             <div class="col-lg-6 col-md-6 mb-4">
                 <div class="card" style="height: 16rem; width: 18rem;">
-                    <a href="_authors/anderson/index.php"> <img class="card-img-top rounded" src="_images/estilos/throwup.png" alt="..."></a>
+                    <a href="_authors/anderson/index.php"> <img class="card-img-top rounded" src="_images/estilos/throwup.png" alt="Imagem capa Grafite Throwup"></a>
                     <div class="card-body">
                     <a href="_authors/anderson/index.php"><p class="card-text text-right"><strong>Saiba mais</strong></p>
                     </div>
@@ -162,7 +164,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-4 col-md-4">
                 <div class="card" style="height: 16rem; width: 18rem;">
-                    <a href="_authors/matheus/index.php"> <img class="card-img-top rounded" src="_images/estilos/stencil.png" alt="..."></a>
+                    <a href="_authors/matheus/index.php"> <img class="card-img-top rounded" src="_images/estilos/stencil.png" alt="Imagem capa Grafite Stencil"></a>
                     <div class="card-body">
                     <a href="_authors/matheus/index.php"><p class="card-text text-right"><strong>Saiba mais</strong></p>
                     </div>
@@ -170,7 +172,7 @@
             </div>
             <div class="col-lg-4 col-md-4">
                 <div class="card" style="height: 16rem; width: 18rem;">
-                    <a href="_authors/larissa/index.php"><img class="card-img-top rounded" src="_images/estilos/mural.png" alt="..."></a>
+                    <a href="_authors/larissa/index.php"><img class="card-img-top rounded" src="_images/estilos/mural.png" alt="Imagem capa Grafite Mural"></a>
                     <div class="card-body">
                     <a href="_authors/larissa/index.php"><p class="card-text text-right"><strong>Saiba mais</strong></p>
                     </div>
@@ -178,7 +180,7 @@
             </div>
             <div class="col-lg-4 col-md-4">
                 <div class="card" style="height: 16rem; width: 18rem;">
-                    <a href="_authors/adelina/index.php"><img class="card-img-top rounded" src="_images/estilos/3d.png" alt="Imagem de capa 3D"></a>
+                    <a href="_authors/adelina/index.php"><img class="card-img-top rounded" src="_images/estilos/3d.png" alt="Imagem capa Grafite  3D"></a>
                     <div class="card-body">
                         <a href="_authors/adelina/index.php"><p class="card-text text-right"><strong>Saiba mais</strong></p>
                     </div>
