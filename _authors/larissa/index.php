79289691dbff02d7115d2693367587e1d5c964d6
MOREIRALARISSA <lasouza.martins@hotmail.com>
2019-12-03 18:08:46 -0300

    teste

--- _authors/larissa/index.php
+++ _authors/larissa/index.php
@@ -140,8 +140,8 @@
                                                     <img src="../../_images/larissa/cricaperfil.jpg" alt="Crica" class="img-fluid">
                                                 </div>
                                                 <div class="card-body">
-                                                    <h2 class="card-title font-weight-bold">Crica Monteiro</h2>
-                                                    <h3 class="card-subtitle mb-2 text-muted">Cristiane Monteiro</h3>
+                                                    <h3 class="card-title font-weight-bold">Crica Monteiro</h3>
+                                                    <h4 class="card-subtitle mb-2 text-muted">Cristiane Monteiro</h4>
                                                     <p class="card-text">Comecou a grafitar ainda na adolescência e hoje é ilustratoda na MSP.</p>
                                                 </div>
                                                 <ul class="list-group list-group-flush">
