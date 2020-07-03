16a0339fefc107c7c60e5a44cc9f09cd3dec1756
unknown <andersonlantunes@gmail.com>
2020-07-03 18:11:23 -0300

    commit

--- index.php
+++ index.php
@@ -12,13 +12,15 @@
         <div class="row">
 
             <div class="col-lg-4 col-md-4 col-md-4 mt-5">
-                <h1 class="d-none"> <a href="quem-somos.php"> Das Ruas para Web </a></h1>
-                <h2 class="text-center mt-5"> <a href="quem-somos.php"> Das Ruas para Web </a></h2>
+                <a href="quem-somos.php">
+                    <h1 class="d-none"> Das Ruas para Web </h1>
+                    <h2 class="text-center mt-5"> Das Ruas para Web </h2>
+                </a>
                 <p class="text-left mb-5">
                     Portal sobre <strong>GRAFITE</strong> desenvolvido pelos estudantes do curso sistemas para internet da FATEC São Roque, o grupo é composto por cinco integrantes: Adelina, Anderson, Igor, Larissa e Matheus.
                 </p>
 
-                <h2 class="text-center mt-5"> <a href="grafite-no-mundo.php">Grafite no Mundo </a></h2>
+                <h2 class="text-center mt-5"> Grafite no Mundo </h2>
                 <p class="text-left">
                      Ao redor do mundo é possível observar grandes artistas que levam a sério a arte de rua e transformam “lugares normais” em verdadeiras telas.<a href="grafite-no-mundo.php"> Confira! </a>
                 
@@ -31,13 +33,17 @@
                 <div class="col-lg-4 col-md-4 mt-5">
 
                     <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
-                     <a href="grafite-x-sociedade.php"> <img class="img-fluid rounded col-12" src="./_images/grafite-x-sociedade/hellsKit1970s.jpg" alt="Grafite feito em Hells Kit em 1970"> </a>
-                     <h2 class="text-center txtimg"><a href="grafite-x-sociedade.php">O que é o Grafite ?</a></h2>
+                        <a href="grafite-x-sociedade.php"> 
+                            <img class="img-fluid rounded col-12" src="./_images/grafite-x-sociedade/hellsKit1970s.jpg" alt="Grafite feito em Hells Kit em 1970"> 
+                            <h2 class="text-center txtimg">O que é o Grafite ?</h2>
+                        </a>
                     </div> 
 
                     <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
-                    <a href="historia-do-grafite.php"><img class="img-fluid rounded col-12" src="./_images/historia-do-grafite/grafite1982.jpg" alt="Um dos primeiros Grafites da cidade de São Paulo"> </a>
-                    <h2 class="text-center txtimg"><a href="historia-do-grafite.php">História do Grafite</a></h2>
+                        <a href="historia-do-grafite.php">
+                            <img class="img-fluid rounded col-12" src="./_images/historia-do-grafite/grafite1982.jpg" alt="Um dos primeiros Grafites da cidade de São Paulo">
+                            <h2 class="text-center txtimg"> História do Grafite </h2>
+                        </a>
                     </div> 
 
                 </div>
@@ -45,13 +51,17 @@
                 <div class="col-lg-4 col-md-4 mt-5">
 
                     <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
-                    <a href="mitos-e-verdades.php"> <img class="img-fluid rounded col-12" src="./_images/mitos-e-verdades/pichacao-centro-bh.jpg" alt="Pichação no centro da cidade de Belo Horizonte"> </a>
-                    <h2 class="text-center txtimg"><a href="mitos-e-verdades.php">Pichação é crime!</a></h2>
+                        <a href="mitos-e-verdades.php">
+                            <img class="img-fluid rounded col-12" src="./_images/mitos-e-verdades/pichacao-centro-bh.jpg" alt="Pichação no centro da cidade de Belo Horizonte">
+                            <h2 class="text-center txtimg"> Pichação é crime! </h2>
+                        </a>
                     </div>  
 
                     <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
-                    <a href="grafite-no-mundo.php"><img class="img-fluid rounded col-12" src="./_images/grafite-no-mundo/kobra.jpg" alt="Grafite feito pelo artista Eduardo Kobra"> </a>
-                    <h2 class="text-center txtimg"><a href="grafite-no-mundo.php">Grafite no Mundo</a></h2>
+                        <a href="grafite-no-mundo.php">
+                            <img class="img-fluid rounded col-12" src="./_images/grafite-no-mundo/kobra.jpg" alt="Grafite feito pelo artista Eduardo Kobra">
+                            <h2 class="text-center txtimg"> Grafite no Mundo </h2>
+                        </a>
                     </div>            
 
                 </div>
@@ -63,13 +73,11 @@
                     <div class="row m-1">
                         <div class="col-md-12 col-lg-6">
                             <a href="news.php">
-                            <img src="./_images/news/museuderua.jpg" class="d-block w-100 roudend img-fluid" alt="Projeto Museu de Rua BH">
+                                <img src="./_images/news/museuderua.jpg" class="d-block w-100 roudend img-fluid" alt="Projeto Museu de Rua BH">
                             </a>
                         </div>
                         <div class="col">
-                            <a href="news.php">
                                 <h3 class="titulo2-news text-center">Museu de Rua</h3>
-                            </a>
                             <p class="text-left">
                             Uma iniciativa inédita de um coletivo de produtores culturais e artistas irá transformar ruas, praças e avenidas de Belo Horizonte em um galeria de arte a céu aberto. Neste sábado (7), das 10h às 19h, será a estreia do Museu de Rua! 
                             </p>
@@ -80,13 +88,11 @@
                     <div class="row m-1">
                         <div class="col-md-12 col-lg-6">
                             <a href="news.php">
-                              <img class="d-block w-100 roudend img-fluid" src="./_images/news/osgemeos.jpg" alt="Irmãos Gustavo e Otávio Pandolfo na Pinacoteca"/>
+                                <img class="d-block w-100 roudend img-fluid" src="./_images/news/osgemeos.jpg" alt="Irmãos Gustavo e Otávio Pandolfo na Pinacoteca"/>
                             </a>
                         </div>
                         <div class="col">
-                            <a href="news.php">
                                 <h3 class="titulo2-news">Os Gêmeos na Pinacoteca em 2020</h3>
-                            </a>
                             <p class="text-left">
                             Os Gêmeos, grafiteiros brasileiros que conquistaram todo o mundo com sua street art conquistaram um espaço para terem suas obras expostas na galeria da Pinacoteca.
                             </p>
@@ -128,12 +134,14 @@
                 
             </div>
             <div class="col-3">
-                <figure>
-                <a href="_authors/adelina/index.php"><img src="./_images/matheus/izolag.jpg" alt="Artista Izolag"></a>
-                    <figcaption>
-                        <strong>Izolag</strong>
-                    </figcaption>
-                </figure>
+                <a href="_authors/adelina/index.php">
+                    <figure>
+                        <img src="./_images/matheus/izolag.jpg" alt="Artista Izolag">
+                        <figcaption>
+                            <strong>Izolag</strong>
+                        </figcaption>
+                    </figure>
+                </a>
             </div>
         </div>
         <div>
