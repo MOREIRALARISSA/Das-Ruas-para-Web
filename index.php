7737ed96946321f061bfdfa56cabca62656cb47e
Larissa Moreira <lasouza.martins@hotmail.com>
2020-07-05 17:09:03 -0300

    role

--- index.php
+++ index.php
@@ -8,20 +8,20 @@
         <div class="row">
             <article role="article">
                 <div class="col-lg-4 col-md-4 col-md-4 mt-5">
-                    <a href="quem-somos.php">
-                        <h1 class="text-center mt-5">
-                            <h2 class=""> Das Ruas para Web </h2>
+                    <div>
+                        <h1 class="text-center mt-5" role="heading">
+                        <a href="quem-somos.php">   <h2 class="" role="heading" aria-level="1"> Das Ruas para Web </h2> </a>
                         </h1>
-                    </a>
-                    <p class="text-left mb-5">
+                        <p class="text-left mb-5">
                         Portal sobre <strong>GRAFITE</strong> desenvolvido pelos estudantes do curso sistemas para internet da FATEC São Roque, o grupo é composto por cinco integrantes: Adelina, Anderson, Igor, Larissa e Matheus.
-                    </p>
-
-                    <h2 class="text-left mt-5"> Grafite no Mundo </h2>
+                        </p>
+                    </div>
+                    <div>
+                    <h2 class="text-left mt-5" role="heading" aria-level="1"> Grafite no Mundo </h2>
                     <p class="text-left">
                         Ao redor do mundo é possível observar grandes artistas que levam a sério a arte de rua e transformam “lugares normais” em verdadeiras telas.<a href="grafite-no-mundo.php"> Confira! </a>
                     </p>
-
+                    </div>
                 </div>
 
                 <div class="col-lg-4 col-md-4 mt-5">
@@ -29,14 +29,14 @@
                     <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
                         <a href="grafite-x-sociedade.php">
                             <img class="img-fluid rounded col-12" src="./_images/grafite-x-sociedade/hellsKit1970s.jpg" alt="Grafite feito em Hells Kit em 1970">
-                            <h2 class="text-center txtimg">O que é o Grafite ?</h2>
+                            <h2 class="text-center txtimg" role="heading">O que é o Grafite ?</h2>
                         </a>
                     </div>
 
                     <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
                         <a href="historia-do-grafite.php">
                             <img class="img-fluid rounded col-12" src="./_images/historia-do-grafite/grafite1982.jpg" alt="Um dos primeiros Grafites da cidade de São Paulo">
-                            <h2 class="text-center txtimg"> História do Grafite </h2>
+                            <h2 class="text-center txtimg" role="heading"> História do Grafite </h2>
                         </a>
                     </div>
                 </div>
@@ -48,21 +48,21 @@
             <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
                 <a href="mitos-e-verdades.php">
                     <img class="img-fluid rounded col-12" src="./_images/mitos-e-verdades/pichacao-centro-bh.jpg" alt="Pichação no centro da cidade de Belo Horizonte">
-                    <h2 class="text-center txtimg"> Pichação é crime! </h2>
+                    <h2 class="text-center txtimg" role="heading"> Pichação é crime! </h2>
                 </a>
             </div>
 
             <div class="col-lg-9 offset-lg-2 col-md-12 mt-md-5">
                 <a href="grafite-no-mundo.php">
                     <img class="img-fluid rounded col-12" src="./_images/grafite-no-mundo/kobra.jpg" alt="Grafite feito pelo artista Eduardo Kobra">
-                    <h2 class="text-center txtimg"> Grafite no Mundo </h2>
+                    <h2 class="text-center txtimg" role="heading"> Grafite no Mundo </h2>
                 </a>
             </div>
 
         </div>
 
         <div>
-            <h2 class="text-left">News</h2>
+            <h2 class="text-left" role="heading">News</h2>
             <div class="row m-1">
                 <div class="col-sm-6">
                     <div class="row m-1">
@@ -72,7 +72,7 @@
                             </a>
                         </div>
                         <div class="col">
-                            <h3 class="titulo2-news text-center">Museu de Rua</h3>
+                            <h3 class="titulo2-news text-center" role="heading">Museu de Rua</h3>
                             <p class="text-left">
                                 Uma iniciativa inédita de um coletivo de produtores culturais e artistas irá transformar ruas, praças e avenidas de Belo Horizonte em um galeria de arte a céu aberto. Neste sábado (7), das 10h às 19h, será a estreia do Museu de Rua!
                             </p>
@@ -87,7 +87,7 @@
                             </a>
                         </div>
                         <div class="col">
-                            <h3 class="titulo2-news">Os Gêmeos na Pinacoteca em 2020</h3>
+                            <h3 class="titulo2-news" role="heading">Os Gêmeos na Pinacoteca em 2020</h3>
                             <p class="text-left">
                                 Os Gêmeos, grafiteiros brasileiros que conquistaram todo o mundo com sua street art conquistaram um espaço para terem suas obras expostas na galeria da Pinacoteca.
                             </p>
@@ -99,7 +99,7 @@
         </div>
         <div>
             <div class="">
-                <h2 class="text-left mt-5 mb-3">Artistas</h2>
+                <h2 class="text-left mt-5 mb-3" role="heading">Artistas</h2>
                 <div class="row m-1">
                     <div class="col-3">
                         <figure>
@@ -136,7 +136,7 @@
                 </div>
             </div>
             <div>
-                <h2 class="text-left mt-4 mb-4">Estilos</h2>
+                <h2 class="text-left mt-4 mb-4" role="heading">Estilos</h2>
                 <div class="row mt-4">
                     <div class="col-lg-3 offset-lg-2 col-md-3 offset-md-2 col-sm-10 offset-sm-1 col-10 offset-1 mb-4">
                         <div class="card">
